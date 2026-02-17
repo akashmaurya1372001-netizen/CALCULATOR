4f599095fd9ff7a47374e98b266349f6f8656155
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-02-17 22:20:38 +0530

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 function App() {
-  return(
-  <div className={styles.calculator}>
-    <input className={styles.display} id="display" type="text" />
-    <div id="buttons-container">
-      <button>C</button>
+  return (
+    <div className={styles.calculator}>
+      <input className={styles.display} id="display" type="text" />
+      <div className="button-Container" id="buttons-container">
+        <button>C</button>
+      </div>
     </div>
-  </div>
-);}
+  );
+}
 export default App;
